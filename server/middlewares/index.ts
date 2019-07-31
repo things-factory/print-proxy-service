e591cc9d9922b30a8843646395d791efce85ab1e
Stellan Gil <lotstar87@users.noreply.github.com>
2019-07-31 16:56:03 +0900

    browse all services when service down detected

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -1,14 +1,25 @@
 var bonjour = require('bonjour')()
+var browser
+
 process.on('bootstrap-module-middleware' as any, app => {
-  var browser = bonjour.find(
-    {
-      type: 'tfprinter',
-      protocol: 'tcp'
-    },
-    service => {
-      ;(global as any).printers = browser.services
-    }
-  )
+  browser = bonjour.find({
+    type: 'tfprinter',
+    protocol: 'tcp'
+  })
+
+  browser.on('up', service => {
+    ;(global as any).printers = browser.services
+  })
+  browser.on('down', service => {
+    ;(global as any).printers = browser.services
+  })
 
   browser.start()
+})
+
+process.on('exit' as any, code => {
+  if (browser) {
+    browser.stop()
+    browser = null
+  }
 })
