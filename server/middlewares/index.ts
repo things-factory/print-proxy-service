b4326b167a0a0310e989d6348ea13f2e3996a140
Hearty, Oh <heartyoh@hatiolab.com>
2020-09-16 08:10:01 +0900

    try..catch for mdns fault

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -1,26 +1,32 @@
 var mdns = require('mdns')
 var browser
-
 var printers = {}
+
 process.on('bootstrap-module-middleware' as any, async app => {
-  var sequence = [
-    mdns.rst.DNSServiceResolve(),
-    'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({ families: [4] }),
-    mdns.rst.makeAddressesUnique()
-  ]
+  try {
+    var sequence = [
+      mdns.rst.DNSServiceResolve(),
+      'DNSServiceGetAddrInfo' in mdns.dns_sd
+        ? mdns.rst.DNSServiceGetAddrInfo()
+        : mdns.rst.getaddrinfo({ families: [4] }),
+      mdns.rst.makeAddressesUnique()
+    ]
 
-  browser = mdns.createBrowser(mdns.tcp('tfprinter'), { resolverSequence: sequence })
+    browser = mdns.createBrowser(mdns.tcp('tfprinter'), { resolverSequence: sequence })
 
-  browser.on('serviceUp', service => {
-    printers[service.name] = service
-    ;(global as any).printers = [...Object.values(printers)]
-  })
-  browser.on('serviceDown', service => {
-    delete printers[service.name]
-    ;(global as any).printers = [...Object.values(printers)]
-  })
+    browser.on('serviceUp', service => {
+      printers[service.name] = service
+      ;(global as any).printers = [...Object.values(printers)]
+    })
+    browser.on('serviceDown', service => {
+      delete printers[service.name]
+      ;(global as any).printers = [...Object.values(printers)]
+    })
 
-  browser.start()
+    browser.start()
+  } catch (e) {
+    console.error(e)
+  }
 })
 
 process.on('exit' as any, code => {
